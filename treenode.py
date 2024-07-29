b620ba1cb72ce246fb309cd3b199c74579daa212
sana khan <88804450+sanatiwary@users.noreply.github.com>
2024-07-28 20:03:44 -0700

    Add files via upload

--- treenode.py
+++ treenode.py
@@ -25,18 +25,21 @@ def postOrderTrav(node):
         postOrderTrav(node.right)
         print(node.val, end=", ")
 
-
 root = TreeNode("A")
 root.left = TreeNode("B")
 root.right = TreeNode("C")
 root.left.left = TreeNode("D")
 root.left.right = TreeNode("E")
 
-print("\n in order traversal: ")
-inOrderTrav(root)
-
-print("\n pre order traversal: ")
-preOrderTrav(root)
-
-print("\n post order traversal: ")
-postOrderTrav(root)
+key = int(input("enter 1 for in order traversal, 2 for pre order traversal, or 3 for post order traversal: "))
+if key == 1:
+    print("\n in order traversal: ")
+    inOrderTrav(root)
+elif key == 2:
+    print("\n pre order traversal: ")
+    preOrderTrav(root)
+elif key == 3:
+    print("\n post order traversal: ")
+    postOrderTrav(root)
+else:
+    print("please enter a valid option")
